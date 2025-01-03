10389e94a5e5d4fe598f2ec66d56c0ed92876e21
MiiKY <mikel.ra.2003@gmail.com>
2025-01-03 16:35:08 +0100

    add:añadida la conexion a la bbdd

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.mikel.dein_jasper_2 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     opens org.mikel.dein_jasper_2.controladores to javafx.fxml;
     opens org.mikel.dein_jasper_2 to javafx.fxml;
